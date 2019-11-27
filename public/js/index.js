1065e212336a5700caaf557c5c5821df92e3b01f
Amedee <amedeekirk@yahoo.com>
2019-11-26 17:46:41 -0800

    code cleanup

--- public/js/index.js
+++ public/js/index.js
@@ -1,152 +1,167 @@
-var socket = io();
+/* global io */
 
-window.onload = function() {
-    var canvas = $('#whiteboard')[0];
-    var context = canvas.getContext('2d');
-    var current = {color: '#000000'};
-    var drawing = false;
-    var throttle = false;
+const socket = io();
 
-    var guess = true;
+window.onload = () => {
+  const canvas = $('#whiteboard')[0];
+  const context = canvas.getContext('2d');
+  const current = { color: '#000000' };
+  let drawing = false;
+  let throttle = false;
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    context.lineWidth = 2;
+  let guess = true;
 
-    $('#clear, #brush, #dropdownContent, #userGuess').hide();
-    $('#black').css("border", "2px solid #FFFFFF");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  context.lineWidth = 2;
 
-    $('#whiteboard').mousedown(function (e) {
-        drawing = true;
-        current.x = e.clientX;
-        current.y = e.clientY;
-    }).on('mouseup mouseout', function (e) {
-        if (!drawing) {
-            return;
-        }
-        drawing = false;
-        if(!guess){
-            drawLine(current.x, current.y, e.clientX, e.clientY, current.color, context.lineWidth, true);
-        }
-    }).mousemove(function (e) {
-        if(!throttle){
-            if (!drawing) {
-                return;
-            }
-            if(!guess) {
-                drawLine(current.x, current.y, e.clientX, e.clientY, current.color, context.lineWidth, true);
-            }
-            current.x = e.clientX;
-            current.y = e.clientY;
-            throttle = true;
-            setTimeout(function(){ throttle = false; },10);
-        }
+  $('#clear, #brush, #dropdownContent, #userGuess').hide();
+  $('#black').css('border', '2px solid #FFFFFF');
 
-    });
 
-    function drawLine(x0, y0, x1, y1, color, width, emit) {
-        context.lineWidth = width;
-        context.lineCap = 'round';
-        context.beginPath();
-        context.moveTo(x0, y0);
-        context.lineTo(x1, y1);
-        context.strokeStyle = color;
-        context.stroke();
-        context.closePath();
+  function drawer(word) {
+    guess = false;
+    $('#userGuess').hide();
+    $('#clear, #word, #brush').show();
+    $('#word').text(`Your word is '${word}'`);
+  }
 
-        if (!emit) {
-            return;
-        }
-        var w = canvas.width;
-        var h = canvas.height;
+  function guesser() {
+    guess = true;
+    $('#userGuess').show();
+    $('#clear, #word, #brush, #dropdownContent').hide();
+  }
 
-        socket.emit('drawing_c', {
-            x0: x0 / w,
-            y0: y0 / h,
-            x1: x1 / w,
-            y1: y1 / h,
-            color: color,
-            width: width
-        });
+  function drawLine(x0, y0, x1, y1, color, width, emit) {
+    context.lineWidth = width;
+    context.lineCap = 'round';
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = color;
+    context.stroke();
+    context.closePath();
+
+    if (!emit) {
+      return;
     }
+    const w = canvas.width;
+    const h = canvas.height;
 
-    socket.on('drawer', function (word) {
-        drawer(word);
+    socket.emit('clientEmitDrawing', {
+      x0: x0 / w,
+      y0: y0 / h,
+      x1: x1 / w,
+      y1: y1 / h,
+      color,
+      width,
     });
+  }
 
-    socket.on('guesser', function () {
-        guesser();
-    });
-
-    socket.on('drawing_s', function (data) {
-        var w = canvas.width;
-        var h = canvas.height;
-        drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color, data.width, false);
-    });
-
-    socket.on('guess_s', function (result) {
-        if(result){
-            alert('You Win!');
-        }
-        else{
-            alert("Incorrect!")
-        }
-    });
-
-    socket.on('clear_s', function () {
-        context.clearRect(0, 0, canvas.width, canvas.height)
-    });
-
-    $('#brush').click(function (e) {
-        $('#dropdownContent').toggle();
-        e.stopPropagation();
-    });
-
-    $('#clear').click(function () {
-        socket.emit('clear_c');
-    });
-
-    $('.color-box').click(function () {
-        current.color = '#' + $(this).attr('id');
-        $('.color-box').css('border', '');
-        $(this).css("border", "2px solid #FFFFFF");
-        $('#customColor').val(current.color);
-    });
-
-    $('#brushSlider').on('change', function (e) {
-        context.lineWidth = e.target.value;
-    });
-
-    $('#customColor').on('change', function () {
-        //https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation
-        var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test($(this).val());
-        if(isOk){
-            current.color = $(this).val();
-        }
-        else{
-            $(this).val(current.color);
-        }
-    });
-
-    $('#userGuess').keypress(function(event){
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if(keycode == '13'){
-            socket.emit('guess_c', $(this).val());
-            $(this).val('');
-        }
-
-    });
-
-    function drawer(word){
-        guess = false;
-        $('#userGuess').hide();
-        $('#clear, #word, #brush').show();
-        $('#word').text("Your word is '" + word + "'");
+  function rgb2hex(color) {
+    const rgb = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+    function hex(x) {
+      return (`0${parseInt(x).toString(16)}`).slice(-2);
     }
+    return `#${hex(rgb[1])}${hex(rgb[2])}${hex(rgb[3])}`;
+  }
 
-    function guesser() {
-        guess = true;
-        $('#userGuess').show();
-        $('#clear, #word, #brush, #dropdownContent').hide();
+  $('#whiteboard').mousedown((e) => {
+    drawing = true;
+    current.x = e.clientX;
+    current.y = e.clientY;
+  }).on('mouseup mouseout', (e) => {
+    if (!drawing) {
+      return;
     }
+    drawing = false;
+    if (!guess) {
+      drawLine(current.x, current.y, e.clientX, e.clientY, current.color, context.lineWidth, true);
+    }
+  }).mousemove((e) => {
+    if (!throttle) {
+      if (!drawing) {
+        return;
+      }
+      if (!guess) {
+        drawLine(current.x, current.y,
+          e.clientX, e.clientY,
+          current.color, context.lineWidth,
+          true);
+      }
+      current.x = e.clientX;
+      current.y = e.clientY;
+      throttle = true;
+      setTimeout(() => { throttle = false; }, 10);
+    }
+  });
+
+  socket.on('drawer', (word) => {
+    drawer(word);
+  });
+
+  socket.on('guesser', () => {
+    guesser();
+  });
+
+  socket.on('serverEmitDrawing', (data) => {
+    const w = canvas.width;
+    const h = canvas.height;
+    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color, data.width, false);
+  });
+
+  socket.on('serverEmitGuess', (result) => {
+    if (result) {
+      alert('You Win!');
+    } else {
+      alert('Incorrect!');
+    }
+  });
+
+  socket.on('serverEmitClear', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  $('#brush').click((e) => {
+    $('#dropdownContent').toggle();
+    e.stopPropagation();
+  });
+
+  $('#clear').click(() => {
+    socket.emit('clientEmitClear');
+  });
+
+  $('.color-box').click((e) => {
+    const isEraser = $(e.currentTarget).attr('id') === 'color-eraser';
+    const color = $(e.currentTarget).css('background-color');
+    current.color = isEraser ? '#ffffff' : rgb2hex(color);
+    $('.color-box').css('border', '');
+    $(e.currentTarget).css('border', '2px solid #FFFFFF');
+    $('#customColor').val(current.color);
+  });
+
+  $('#brushSlider').on('change', (e) => {
+    context.lineWidth = e.target.value;
+  });
+
+  $('#customColor').on('change', (e) => {
+    // https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation
+    const isOk = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test($(e.currentTarget).val());
+
+    if (isOk) {
+      current.color = $(e.currentTarget).val();
+      console.log('bloop');
+    } else {
+      $(e.currentTarget).val(current.color);
+      console.log('bleep');
+    }
+  });
+
+  $('#userGuess').keypress((e) => {
+    const keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode === '13') {
+      socket.emit('clientEmitGuess', $(e.currentTarget).val());
+      $(e.currentTarget).val('');
+    }
+  });
 };
--- server.js
+++ server.js
@@ -1,93 +1,94 @@
 const express = require('express');
+
 const app = express();
 const socket = require('socket.io');
+
 const port = process.env.PORT || 4000;
 const words = require('./words');
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
-//App setup
-const server = app.listen(port, function(){
-    console.log('listening for requests on port ' + port);
+// App setup
+const server = app.listen(port, () => {
+  console.log(`listening for requests on port ${port}`);
 });
 
-//Socket setup & pass server
+// Socket setup & pass server
 const io = socket(server);
 
-var clients = [];
-var line_history = [];
-var randomWord;
+const clients = [];
+let lineHistory = [];
+let randomWord;
 
-//Connection handler
-io.on('connection', function (socket) {
+// Connection handler
+io.on('connection', (socket) => {
+  function initialUserRole() {
+    // If only user, generate random word and assign him as 'drawer'
+    if (clients.length === 1) {
+      randomWord = words[Math.floor(Math.random() * words.length)];
+      console.log(`   --New word is: ${randomWord}`);
+      const onlyUser = clients[0].id;
+      lineHistory = [];
+      io.emit('serverEmitClear');
+      io.to(onlyUser).emit('drawer', randomWord);
+    }
+    // Otherwise assign 'guesser' role
+    else {
+      socket.emit('guesser');
+    }
+  }
 
-    //Store socket information and assign role
-    console.log('Made connection with: ', socket.id);
-    clients.push(socket);
+  // Give previously drawn content to new socket
+  function sendCurrentDrawing() {
+    Object.keys(lineHistory).forEach((key) => {
+      socket.emit('serverEmitDrawing', lineHistory[key]);
+    });
+  }
+
+  // Store socket information and assign role
+  console.log('   --Made connection with: ', socket.id);
+  clients.push(socket);
+  initialUserRole();
+  sendCurrentDrawing();
+
+  console.log(`   --Current amount of players: ${clients.length}`);
+
+  // When line comes in from drawer, store and send to other clients
+  socket.on('clientEmitDrawing', (data) => {
+    lineHistory.push(data);
+    socket.broadcast.emit('serverEmitDrawing', data);
+  });
+
+  // If client sends a clear signal, tell all clients to clear canvas
+  socket.on('clientEmitClear', () => {
+    // also empty line history
+    lineHistory = [];
+    io.emit('serverEmitClear');
+  });
+
+  socket.on('clientEmitGuess', (guess) => {
+    // Reduce word and user guess to single word lower-case strings
+    const original = randomWord.toLowerCase().toString().replace(/\s/g, '');
+    const userGuess = guess.toLowerCase().toString().replace(/\s/g, '');
+    if (original === userGuess) {
+      socket.emit('serverEmitGuess', true);
+      randomWord = words[Math.floor(Math.random() * words.length)];
+      console.log(`   --New word is: ${randomWord}`);
+      lineHistory = [];
+      io.emit('serverEmitClear');
+      io.emit('drawer', randomWord);
+      socket.broadcast.emit('guesser');
+    } else {
+      socket.emit('serverEmitGuess', false);
+    }
+  });
+
+  // disconnect handler
+  socket.on('disconnect', () => {
+    console.log('   --client %s has disconnected', socket.id);
+    const i = clients.indexOf(socket);
+    clients.splice(i, 1);
     initialUserRole();
-    sendCurrentDrawing();
-
-    //Give previously drawn content to new socket
-    function sendCurrentDrawing() {
-        for (var i in line_history) {
-            socket.emit('drawing_s', line_history[i]);
-        }
-    }
-
-    //When line comes in from drawer, store and send to other clients
-    socket.on('drawing_c', function(data){
-        line_history.push(data);
-        socket.broadcast.emit('drawing_s', data)
-    });
-
-    //If client sends a clear signal, tell all clients to clear canvas
-    socket.on('clear_c', function () {
-        //also empty line history
-        line_history=[];
-        io.emit('clear_s')
-    });
-
-    socket.on('guess_c', function (guess) {
-        //Reduce word and user guess to single word lower-case strings
-        var original = randomWord.toLowerCase().toString().replace(/\s/g,'');
-        var userGuess = guess.toLowerCase().toString().replace(/\s/g,'');
-        if(original == userGuess){
-            socket.emit('guess_s', true);
-            randomWord = words[Math.floor(Math.random() * words.length)];
-            console.log('New word is: ' + randomWord);
-            line_history = [];
-            io.emit('clear_s');
-            io.emit('drawer', randomWord);
-            socket.broadcast.emit('guesser');
-        }
-        else{
-            socket.emit('guess_s', false);
-        }
-    });
-
-    //disconnect handler
-    socket.on('disconnect', function () {
-        console.log('client %s has disconnected', socket.id);
-        var i = clients.indexOf(socket);
-        clients.splice(i, 1);
-        initialUserRole();
-    });
-
-    function initialUserRole() {
-        //If only user, generate random word and assign him as 'drawer'
-        if(clients.length == 1){
-            randomWord = words[Math.floor(Math.random() * words.length)];
-            console.log('New word is: ' + randomWord);
-            var onlyUser = clients[0].id;
-            line_history = [];
-            io.emit('clear_s');
-            io.to(onlyUser).emit('drawer', randomWord);
-        }
-        //Otherwise assign 'guesser' role
-        else {
-            console.log(clients.length);
-            socket.emit('guesser');
-        }
-    }
+  });
 });
