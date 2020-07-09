e35af8937a732e43442b006427bfa811853b50d1
Amedee <amedeekirk@yahoo.com>
2020-07-09 15:05:31 -0700

    Vue canvas drawing working

--- frontend/src/components/GameWhiteboard.vue
+++ frontend/src/components/GameWhiteboard.vue
@@ -1,16 +1,90 @@
 <template>
-  <canvas class="game-whiteboard">whiteboard</canvas>
+  <canvas class="game-whiteboard"
+          id="canvas"
+          @mousedown="initDrawing"
+          @mouseup="stopDrawing"
+          @mouseleave="stopDrawing"
+          @mousemove="draw">
+    Sorry, your browser does not support HTML5 canvas technology.
+  </canvas>
 </template>
 
 <script>
 export default {
   name: 'GameWhiteboard',
+  data() {
+    return {
+      drawing: false,
+      throttle: false,
+      emit: false,
+      canvas: null,
+      ctx: null,
+      brush: {
+        color: '#000',
+        width: 2,
+        x: null,
+        y: null,
+      },
+    };
+  },
+  mounted() {
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.canvas.style.width = '100%';
+    this.canvas.style.height = '100%';
+    this.canvas.width = this.canvas.offsetWidth;
+    this.canvas.height = this.canvas.offsetHeight;
+  },
+
+  methods: {
+    drawLine(x0, y0, x1, y1) {
+      this.ctx.lineWidth = this.brush.width;
+      this.ctx.lineCap = 'round';
+      this.ctx.beginPath();
+      this.ctx.moveTo(x0, y0);
+      this.ctx.lineTo(x1, y1);
+      this.ctx.strokeStyle = this.brush.color;
+      this.ctx.stroke();
+      this.ctx.closePath();
+
+      if (!this.emit) {
+        // return;
+      }
+    },
+
+    initDrawing(e) {
+      console.log(e);
+      this.brush.x = e.clientX - this.canvas?.offsetLeft;
+      this.brush.y = e.clientY - this.canvas?.offsetTop;
+      this.drawing = true;
+    },
+
+    stopDrawing() {
+      this.drawing = false;
+    },
+
+    draw(e) {
+      // A game loop of sorts
+      if (!this.throttle && this.drawing) {
+        this.drawLine(
+          this.brush.x,
+          this.brush.y,
+          e.clientX - this.canvas?.offsetLeft,
+          e.clientY - this.canvas?.offsetTop,
+        );
+        this.brush.x = e.clientX - this.canvas?.offsetLeft;
+        this.brush.y = e.clientY - this.canvas?.offsetTop;
+        this.throttle = true;
+        setTimeout(() => { this.throttle = false; }, 10);
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
   .game-whiteboard {
-    background-color: #222222;
+    background-color: #cccccc;
     grid-column: board-w-start / board-w-end;
     grid-row: board-h-start / board-h-end;
   }
